3179706379baafc63456678157b51dc5b3027e71
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-10-02 11:04:17 -0400

    setting open weather response data to state- removed local storage fxn

--- src/pages/OpenWeatherPage/OpenWeatherPage.jsx
+++ src/pages/OpenWeatherPage/OpenWeatherPage.jsx
@@ -3,9 +3,8 @@ import * as openWeatherApi from '../../utils/openWeatherApi';
 
 export default function OpenWeatherPage() {
     const [openWeatherData, setOpenWeatherData] = useState(null)
+    const [resData, setResData] = useState(null)
     const [error, setError] = useState('')
-
-    
 
     async function fetchOpenWeatherApi() {
         const openWeatherDataValue = {
@@ -19,16 +18,45 @@ export default function OpenWeatherPage() {
             console.log("openWeatherDataReq", openWeatherDataReq)
             const openWeatherDataRes = await openWeatherDataReq;
             setOpenWeatherData(openWeatherDataRes)
+            const data ={
+                lon: (openWeatherDataRes.coord.lon),
+                lat: (openWeatherDataRes.coord.lat),
+                aqi: (openWeatherDataRes.list[0].main.aqi),
+                co: (openWeatherDataRes.list[0].components.co),
+                no: (openWeatherDataRes.list[0].components.no),
+                no2: (openWeatherDataRes.list[0].components.no2),
+                o3: (openWeatherDataRes.list[0].components.o3),
+                so2: (openWeatherDataRes.list[0].components.so2),
+                pm2_5: (openWeatherDataRes.list[0].components.pm2_5),
+                pm10: (openWeatherDataRes.list[0].components.pm10),
+                nh3: (openWeatherDataRes.list[0].components.nh3),
+                dt: (openWeatherDataRes.list[0].dt),
+
+            }
+            console.log('data', data)
+            setResData(data)
+
+            setData(data)
+
+
+            
             // setError('')
         } catch (err) {
             console.log(err)
             setError(err.message);
         }
+
+        function setData(data) {
+            if (data) {
+                localStorage.setItem('data', 
+                    JSON.stringify(data));
+            }
+        }
     }
 
     return (
         <>
-            <h1>HELLO</h1>'
+            <h1>HELLO</h1>
 
             <button onClick={() => fetchOpenWeatherApi()}> CLICK ME: Search LA coordinates</button>
 
