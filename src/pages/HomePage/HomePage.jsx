29ea1a8bce9c74ed34b69894b19dcbc4c25dda6d
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-10-03 12:04:38 -0400

    updated map and weather info to work and make api calls at moveend

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import NavBar from "../../components/NavBar";
 import GeoLocationTest from "../../components/GeoLocationTest";
+import OpenWeatherPage from '../OpenWeatherPage/OpenWeatherPage'
+
 import styles from "../../CSS/home.module.css"
 export default function HomePage(){
     return(
     <div className={styles.homePg}>
         <NavBar></NavBar>
         <GeoLocationTest></GeoLocationTest>
+        <OpenWeatherPage />
     </div>
     )
 }
--- src/pages/OpenWeatherPage/OpenWeatherPage.jsx
+++ src/pages/OpenWeatherPage/OpenWeatherPage.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
 import * as openWeatherApi from '../../utils/openWeatherApi';
+import Map from "../../components/Map/Map"
 
 export default function OpenWeatherPage() {
     const [openWeatherData, setOpenWeatherData] = useState(null)
+    const [resData, setResData] = useState(null)
     const [error, setError] = useState('')
+    const [coordinates, setCoordinates] = useState({ lon: -118.25, lat: 34.05 });
 
-    
+    useEffect(() => {
+        if (coordinates.lon && coordinates.lat) {
+            fetchOpenWeatherApi();
+        }    
+    }, [coordinates]);
+
 
     async function fetchOpenWeatherApi() {
         const openWeatherDataValue = {
@@ -13,12 +21,29 @@ export default function OpenWeatherPage() {
             lon: "-118.25"
         }
         try {
-            const openWeatherDataReq = await openWeatherApi.fetchOpenWeatherData(openWeatherDataValue)
-            try { if (!openWeatherDataReq.ok) throw new Error('Error fetching data'); }
+            const openWeatherDataReq = await openWeatherApi.fetchOpenWeatherData(coordinates)
+            try { 
+                if (!openWeatherDataReq.ok) throw new Error('Error fetching data'); }
             catch (error) { console.log(error) }
             console.log("openWeatherDataReq", openWeatherDataReq)
             const openWeatherDataRes = await openWeatherDataReq;
             setOpenWeatherData(openWeatherDataRes)
+            const data ={
+                lon: (openWeatherDataRes.coord.lon),
+                lat: (openWeatherDataRes.coord.lat),
+                aqi: (openWeatherDataRes.list[0].main.aqi),
+                co: (openWeatherDataRes.list[0].components.co),
+                no: (openWeatherDataRes.list[0].components.no),
+                no2: (openWeatherDataRes.list[0].components.no2),
+                o3: (openWeatherDataRes.list[0].components.o3),
+                so2: (openWeatherDataRes.list[0].components.so2),
+                pm2_5: (openWeatherDataRes.list[0].components.pm2_5),
+                pm10: (openWeatherDataRes.list[0].components.pm10),
+                nh3: (openWeatherDataRes.list[0].components.nh3),
+                dt: (openWeatherDataRes.list[0].dt),
+            }
+            console.log('data', data)
+            setResData(data)
             // setError('')
         } catch (err) {
             console.log(err)
@@ -28,17 +53,12 @@ export default function OpenWeatherPage() {
 
     return (
         <>
-            <h1>HELLO</h1>'
-
-            <button onClick={() => fetchOpenWeatherApi()}> CLICK ME: Search LA coordinates</button>
-
+            <Map setCoordinates={setCoordinates}  />
+            {/* <button onClick={() => fetchOpenWeatherApi()}> CLICK ME: Search LA coordinates</button> */}
             <div >
-
                 {(openWeatherData) ? (JSON.stringify(openWeatherData))
                     : "Need to search something!"}
             </div>
         </>
     )
-
-
 }
