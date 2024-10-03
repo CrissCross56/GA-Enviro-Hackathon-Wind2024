3e795a07a30dfa2e5bf67cad4ae7356d927e9bdf
CrissCross56 <grant.terdoslavich@gmail.com>
2024-10-03 10:43:08 -0400

    started sttylizing a navbar and homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import OpenWeatherPage from './pages/OpenWeatherPage/OpenWeatherPage'
 import "./App.css";
-import GeoLocationTest from "./components/GeoLocationTest";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage"
 
 function App() {
   return (
     <div>
     <Routes>
-      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
+      <Route path="/" element={<HomePage></HomePage>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/openweather" element={<OpenWeatherPage />} />
     </Routes>
-    <GeoLocationTest/>
 
     </div>
   );
