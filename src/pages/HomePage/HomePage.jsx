ae8056355ab9522ba895ab3d411a98c5278bafcb
Tristanh77 <tristanhartman77@gmail.com>
2024-10-04 01:47:29 -0400

    working and responsive aqi component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NavBar from "../../components/NavBar";
 import GeoLocationTest from "../../components/GeoLocationTest";
+import AQIComponent from '../../components/AQIComponent/AQIComponent';
 import OpenWeatherPage from '../OpenWeatherPage/OpenWeatherPage';
 import HealthRecComponent from "../../components/HealthRecComponent/HealthRecComponent";
 import styles from "../../CSS/home.module.css";
@@ -17,6 +18,8 @@ export default function HomePage() {
             <OpenWeatherPage setAqiData={setAqiData} />
             {/* Pass aqiData to HealthRecComponent */}
             <HealthRecComponent aqiData={aqiData} />
+            {/* Pass aqiData to AQIComponent as 'data' */}
+            <AQIComponent data={aqiData} />
         </div>
     );
 }
