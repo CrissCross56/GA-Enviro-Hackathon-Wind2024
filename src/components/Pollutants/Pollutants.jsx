88331f992ff3098dbdb58fbf34107603791f07ef
CrissCross56 <grant.terdoslavich@gmail.com>
2024-10-06 05:38:28 -0400

    added more styles to the component for the pollutants and finished off a progress bar component for more pollutants, also made the color of the progress bars change for when the value of the pollutant being measured  changes

--- src/components/Pollutants/Pollutants.jsx
+++ src/components/Pollutants/Pollutants.jsx
@@ -9,8 +9,12 @@ export default function Pollutants(props) {
         <div className={styles.pollutants}>
              <h1 className={styles.title}>Pollutants</h1>
              <h3 className={styles.more}>Learn More</h3>
-            <ProgressBar name={'Fine Particle Matter'} pollutant={`PM 2.5`} value={80} max={500} concentration={80} units={`μg/m3`}></ProgressBar>
-          
+            <ProgressBar name={'Fine Particle Matter'} pollutant={`PM 2.5`} value={250} max={500} concentration={13} units={`μg/m3`} good={12} moderate={35.4} unhealthySens={55.4} unhealthy={150.4} veryUnhealthy={250.4} hazardous={500}></ProgressBar>
+            {/* make a horizontal grey line here */}
+            <hr className={styles.separator}/>
+            <ProgressBar name={'Coarse Particle Matter'} pollutant={`PM 10`} value={400} max={604} concentration={400} units={`μg/m3`} good={54} moderate={154} unhealthySens={254} unhealthy={354} veryUnhealthy={424} hazardous={604}></ProgressBar>
+            <hr className={styles.separator}/>
+            <ProgressBar name={'Carbon Dioxide'} pollutant={`NO2`} value={400} max={1250} concentration={400} units={`ppb`} good={53} moderate={100} unhealthySens={360} unhealthy={649} veryUnhealthy={1249} hazardous={1250}></ProgressBar>
         </div>
     )
  }
--- src/components/Pollutants/ProgressBar.jsx
+++ src/components/Pollutants/ProgressBar.jsx
@@ -1,5 +1,6 @@
+import { set } from "mongoose";
 import styles from "../../CSS/progress.module.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function ProgressBar(props) {
     // console.log(`hey im in the progressbar.jsx here are my props for value ${props.value} and max ${props.max}`)
     // return (
@@ -14,23 +15,74 @@ export default function ProgressBar(props) {
     //     />
     // </div>
     // )
+    const good = props.good;
+    const moderate = props.moderate;
+    const unhealthySens = props.unhealthySens;
+    const unhealthy = props.unhealthy;
+    const veryUnhealthy = props.veryUnhealthy;
+    const hazardous = props.hazardous;
+
+    const goodColor = '#71C09B';
+    const moderateColor = '#ADEABA';
+    const unhealthySensColor = '#EECF9A';
+    const unhealthyColor = '#F2AD95';
+    const veryUnhealthyColor = '#EE7E90';
+    const hazardousColor = '#BBA3DC';
+
+    const [barColor, setBarColor] = useState(goodColor);
+
+    useEffect(()=>{
+        if(props.value >= 0 && props.value <= good){
+            setBarColor(goodColor)
+        }
+        if(props.value > good && props.value <=moderate){
+            setBarColor(moderateColor)
+        }
+        if(props.value > moderate && props.value <=unhealthySens){
+            setBarColor(unhealthySensColor)
+        }
+        if(props.value > unhealthySens && props.value <= unhealthy){
+            setBarColor(unhealthyColor)
+        }
+        if(props.value > unhealthy && props.value <= veryUnhealthy){
+            setBarColor(veryUnhealthyColor)
+        }
+        if(props.value > veryUnhealthy && props.value <= hazardous){
+            setBarColor(hazardousColor)
+        }
+    })
+
     const progressPercentage = (props.value / props.max) * 100; // Calculate the width of the filled bar based on the current value
 
     return (
-        <div>
-            <h3>{props.name}</h3>
-            <p>{props.pollutant}</p>
+        <div className={styles.comp}>
+            <div className={styles.pollDiv}>
+                <h3 className={styles.name}>{props.name}</h3>
+                <p className={styles.pollutant}>{props.pollutant}</p>
+            </div>
+           
             <div>
                 <div className={styles.progressContainer}>
                     <div
                         className={styles.progressBar}
-                        style={{ width: `${progressPercentage}%` }}
+                        style={{ width: `${progressPercentage}%`,
+                                 backgroundColor: barColor
+                        }}
                     />
                 </div>
-                
+                {/* put the numbers for the scale here */}
+                <div className={styles.range}>
+                    <p className={styles.zero}>0</p>
+                    <p className={styles.maxNumRange}>{props.max}</p>
+                </div>
+               
             </div>
-            <h3>Concentration</h3>
-            <p><span>{props.concentration}</span>{props.units}</p>
+            <div className={styles.concentrationDiv}> 
+                <h3 className={styles.concentrationLabel}>Concentration</h3>
+                <p className={styles.concentrationUnits}><span className={styles.concentration}>{props.concentration}</span>{props.units}</p>
+            </div>
+           
+         
         </div>
     );
  }
