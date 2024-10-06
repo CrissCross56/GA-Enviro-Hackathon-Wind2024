51ad0c887c658d1416f58038826981b647de617c
CrissCross56 <grant.terdoslavich@gmail.com>
2024-10-06 17:34:23 -0400

    started up more work on the location bar, and completed more styling and functionality for the pollutants component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,10 +2,12 @@ import NavBar from "../../components/NavBar";
 import GeoLocationTest from "../../components/GeoLocationTest";
 import styles from "../../CSS/home.module.css"
 import Pollutants from "../../components/Pollutants/Pollutants";
+import LocationBar from "../../components/locationBar/LocationBar";
 export default function HomePage(){
     return(
     <div className={styles.homePg}>
         <NavBar></NavBar>
+        <LocationBar/>
         {/* have placeholder values for the pollutants for a later api call result */}
         <Pollutants pm25={36} pm10={300} n02={500}></Pollutants>
         <GeoLocationTest></GeoLocationTest>
