72a16f177d8aa18739df9d523da958298c1c4900
CrissCross56 <grant.terdoslavich@gmail.com>
2024-10-06 21:32:01 -0400

    made the safety ranges bar, still need to further style it tho

---
+++ src/components/SafetyRanges/SafetyRanges.jsx
@@ -0,0 +1,20 @@
+import  "./safetyRange.module.css"
+export default function SafetyRanges(){
+    return(
+        <div className="safetyBox">
+            <h1>Safety Ranges</h1>
+
+            <div><p>Good/Safe</p></div>
+
+            <div><p>Moderate/Acceptable</p></div>
+
+            <div><p>Unhealthy for Sensitive</p></div>
+
+            <div><p>Unhealthy</p></div>
+
+            <div><p>Very Unhealthy</p></div>
+
+            <div><p>Dangerous</p></div>
+        </div>
+    )
+}
